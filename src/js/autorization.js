fde299bd982761804d0e1c66bf11c3eeb8e30723
olena-semenenko <elen.kinakh@gmail.com>
2023-01-29 03:33:47 +0100

    Update autorization.js

--- src/js/autorization.js
+++ src/js/autorization.js
@@ -22,14 +22,16 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 // Initialize Firebase
 
-createUserWithEmailAndPassword(auth, email, password)
-  .then(userCredential => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch(error => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then(userCredential => {
+//     // Signed in
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch(error => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
+
+console.log('I am work');
